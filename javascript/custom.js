aa20b04050d928d650fdff70a721e9584fc54ef1
Tim Nguyen <timmyn7@gmail.com>
2018-08-13 22:33:51 -0400

    Javascript

---
+++ javascript/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $('a[data-toggle="list"]').on('shown.bs.tab', function(e) {
+        var id = $(e.target).attr('id');
+        if (id == 'list-chubb-list') {
+            $('#work-image').attr('src','images/chubb-logo.jpg')
+        }
+        else if (id =='list-morgan-list') {
+            $('#work-image').attr('src','images/morgan-logo.png')
+        }
+
+        else if (id = 'list-ericsson-list') {
+            $('#work-image').attr('src','images/ericsson-logo.png')
+        }
+    });
+});
